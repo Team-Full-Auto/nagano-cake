3d10688e12311386495c91d36d2678a010b76a91
Masaya Ohtsuka <crystarmen125@gmail.com>
2023-05-19 08:38:12 +0000

    オーダールートnewcreate

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,6 +4,24 @@ def new
 end
 def create
   cart_items = current_custamer.cart_item.all
-  @order = current_custamer.orders.new
+  @order = current_custamer.orders.new(order_params)
+  if@order.save
+    cart_items.each do|cart|
+    order_item=OrderItem.new
+    order_item.item_id = cart.item_id
+    order_item.order_id = @order.id
+    order_item.order_quantity = cart.quantity
+  　 order_item..order_price = cart.item.price
+    order_item.save
+  end
+    redirect_to
+    cart_items.destroy_all
+  else
+      @order = Order.new(order_params)
+      render :new
+  end
 end
+
 end
+
+
