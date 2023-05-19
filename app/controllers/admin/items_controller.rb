0c4fb37fd33d4d83d1242918da2933555949eb59
tackwan <129726011+tackwan@users.noreply.github.com>
2023-05-19 09:31:14 +0000

    [add]items/index,new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,24 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @form = Item.new
+  end
+
+  def create
+    @form = Item.new(item_params)
+    @form.admin_id = current_user.id
+    @form.save
+    redirect_to admin_item_path(@form)
+  end
+
+  def index
+    @items = Item.page(params[:page])
+    @form = Item.new
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:detail,:price)
+  end
+
 end
