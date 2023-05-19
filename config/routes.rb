8c4202150ede9fe213f45f104d1cf90fa1600227
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-05-19 03:23:20 +0000

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  namespace :admin do
+    root :to => "homes#top"
+    resources
+  end
+  scope module: :public do
+    root :to => "homes#top"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
