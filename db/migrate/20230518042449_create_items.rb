6e8ba04d09d8b498b4b9608d2a944a5a4499c955
tackwan <129726011+tackwan@users.noreply.github.com>
2023-05-19 06:11:55 +0000

    reset

---
+++ db/migrate/20230518042449_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      t.references :genre, foreign_key: true,  null: false
+      t.string :name, null: false
+      t.text :detail, null: false
+      t.integer :price, null: false
+      t.boolean :is_selling, default: true, null: false
+      t.timestamps
+    end
+  end
+end
