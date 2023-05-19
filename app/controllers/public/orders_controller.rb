47172cbc6af8a7ea6be638ea78545c5fd248be44
Masaya Ohtsuka <crystarmen125@gmail.com>
2023-05-19 12:36:18 +0000

    order・cofilm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -21,7 +21,18 @@ def create
       render :new
   end
 end
+def confirm
+ @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
+end
 
+private
+def order_params
+  params.require(:order).permit(:payment_method)
+end
 end
 
 
