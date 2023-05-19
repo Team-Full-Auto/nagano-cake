9216a6cb452e3b6db0c5b67190c70206fa4c3788
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-05-19 04:52:47 +0000

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,26 @@ Rails.application.routes.draw do
   }
   namespace :admin do
     root :to => "homes#top"
-    resources
+    resources :customers, only: [:index,:show,:edit,:update]
+    resources :genres, only: [:index,:create,:edit,:update]
+    resources :items, only: [:index,:new,:create,:show,:edit,:update]
+    resources :orders, only: [:show,:update]
+    resources :order_items, only: [:update]
   end
   scope module: :public do
     root :to => "homes#top"
+    get "about" => "homes#about"
+    resources :customers, only: [:show,:edit,:update]
+    get "check" => "customers#check"
+    patch "withdrawal" => "customers#withdrawal"
+    resources :items, only: [:index,:show]
+    resources :cart_items, only: [:index,:update,:destroy,:create]
+    delete "destroy_all" => "cart_items#destroy_all"
+    resources :orders, only: [:new,:show,:index]
+    post "confirm" => "orders#confirm"
+    get "complete" => "orders#complete"
+    resources :shipping_addresses, only: [:index,:edit,:update,:create,:destroy]
+    
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
