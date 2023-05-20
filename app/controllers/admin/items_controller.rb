688addc096b0c7b2a2edd96f71b5df093072b300
tackwan <129726011+tackwan@users.noreply.github.com>
2023-05-20 08:37:44 +0000

    add items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,20 +5,33 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @form = Item.new(item_params)
-    @form.admin_id = current_user.id
     @form.save
     redirect_to admin_item_path(@form)
   end
 
   def index
-    @items = Item.page(params[:page])
+    @items = Item.all
     @form = Item.new
   end
-
+  
+  def show 
+    @item = Item.find(params[:id])
+  end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end 
+  
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item)
+  end 
+  
   private
 
   def item_params
-    params.require(:item).permit(:name,:detail,:price)
+    params.require(:item).permit(:name,:detail,:price,:image)
   end
 
 end
