6b05fac650e8a4a6d03767e9952c6a63b2aa0fc1
Masaya Ohtsuka <crystarmen125@gmail.com>
2023-05-21 14:46:40 +0000

    修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,12 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_customer!
+
+
+def show
+  
+end
+def update
+     @order.update(order_params)
+    redirect_to admin_order_path(@order), notice: 'Successfully updated order status'
+  end
 end
