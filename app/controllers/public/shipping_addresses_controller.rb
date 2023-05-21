2d404b1831de5e98a2a257e171b06bf4abb6c057
Masaya Ohtsuka <crystarmen125@gmail.com>
2023-05-21 02:26:33 +0000

    shipping_address

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,14 +1,28 @@
 class Public::ShippingAddressesController < ApplicationController
 def index
+  @shipping_addresses = shipping_addresses.all
+  @customer = current_customer
+   @shipping_address = shipping_address.new
 end
 def edit
+  @shipping_address = shipping_address.find(params[:id])
+  if @customer = current_customer
+        render "edit"
+  else
+      redirect_to
+  end
 end
 def create
-end
-def update
   
 end
+def update
+  @shipping_address = shipping_address.find(params[:id])
+   redirect_to
+end
 def destroy
+   shipping_address = shipping_address.find(params[:id])
+   shipping_address.destroy
+   
 end
 
 end
