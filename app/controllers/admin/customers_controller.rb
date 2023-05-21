d5b87c8b3589707cc61afd0c60e4de7dc12eafe2
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-05-21 07:10:11 +0000

    会員一覧作成中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,19 @@
 class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(parmas[:id])
+  end
+
+  def edit
+    @customer = Customer.find(parmas[:id])
+  end
+
+  def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customer_path(cutomer.id)
+  end
 end
