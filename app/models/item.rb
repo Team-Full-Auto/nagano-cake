1a0767ab62385df7e1e608e7a0e0f4b07d993d08
tackwan <129726011+tackwan@users.noreply.github.com>
2023-05-22 06:14:21 +0000

    [add]cart_item

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,9 @@ class Item < ApplicationRecord
   has_many :order_items, dependent: :destroy
 
   has_one_attached :image
+  
+  # 税込価格を求めるメソッド
+  def with_tax_price
+    (price * 1.1).floor
+  end 
 end
