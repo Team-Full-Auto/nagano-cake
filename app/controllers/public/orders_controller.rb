b0e7e06afabe9c93c22797d133ef7eab43980150
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-05-23 05:36:52 +0000

    修正3

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -42,6 +42,7 @@ end
 
 def confirm
  @order = Order.new(order_params)
+  binding.pry 
     @address = Address.find(params[:order][:address_id])
     @order.postal_code = @address.postal_code
     @order.address = @address.address
