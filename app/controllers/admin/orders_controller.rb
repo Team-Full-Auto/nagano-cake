ac7b02207f80a5a9c763d785f79b8a55e08b768f
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-05-24 01:38:47 +0000

    変更3

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,13 +1,25 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_customer!
-def index
-end
+
 
 def show
-  
+
 end
 def update
-     @order.update(order_params)
+    @order.find(params[:id])
+    @order_item =OrderItem.where(order_id: params[:id])
+    if @order.update(order_params)
+      @order_itemupdate_all(making_status: 1)
+
+      if @order.status == "payment_confirmation"
+      end
+    end
+
+
     redirect_to admin_order_path(@order), notice: 'Successfully updated order status'
   end
+  private
+def order_params
+  params.require(:order).permit(:status)
+end
 end
