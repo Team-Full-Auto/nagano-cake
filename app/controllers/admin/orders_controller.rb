bb43025ae43f7288df9469bf23de0c0fcbe1f7bb
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-05-26 10:41:23 +0000

    admin注文履歴詳細作成中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,8 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
 
   def show
-    @orders = Order.find(params[:id])
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
   end
 
   def update
