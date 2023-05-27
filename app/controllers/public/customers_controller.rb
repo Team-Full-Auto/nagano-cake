07aaf0699d5235ade5f7eb1d7e4116b1b1eb8ed2
tackwan <129726011+tackwan@users.noreply.github.com>
2023-05-27 06:34:51 +0000

    [update]admin/order/show status

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -26,7 +26,7 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdrawal
-    @customer = Customer.find(params[:id])
+    @customer = current_customer
     #is_deletedカラムをtrueに変更することにより削除フラグを立てる
     @customer.update(is_deleted: true)
     reset_session
